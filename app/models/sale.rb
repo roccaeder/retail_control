ce9652722953add4b18cc5600eff146a020337a4
Tu Nombre <rocca.eder@gmail.com>
2026-04-02 00:22:00 +0100

    feature: autocorrect with rubocop

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,6 +1,7 @@
 class Sale < ApplicationRecord
   belongs_to :customer
   has_many :sale_items, dependent: :destroy
+  accepts_nested_attributes_for :sale_items, reject_if: :all_blank
 
   enum :status, { paid: 0, pending: 1 }
   enum :payment_method, { cash: 0, credit: 1, transfer: 2 }
