f86ec4dc36c7feb0252db10a602fd61bb68a8b0a
eder <rocca.eder@gmail.com>
2026-04-05 22:39:18 +0100

    feat: fix with rubocop

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,9 @@ class ProductsController < ApplicationController
   def search
     @products = if params[:q].present?
                   Product.where("name ILIKE ? OR sku ILIKE ?", "%#{params[:q]}%", "%#{params[:q]}%").limit(10)
-                else
+    else
                   []
-                end
+    end
 
     render layout: false
   end
