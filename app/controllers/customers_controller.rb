c077a22c981d983b501fd60f2115af7d57fbfe28
eder <rocca.eder@gmail.com>
2026-04-27 02:38:25 +0100

    feat: add crud for customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,55 @@
 class CustomersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_customer, only: %i[show edit update destroy]
   layout "erp"
 
   def index
     @customers = Customer.order(:name)
+  end
+
+  def show
+    @sales = @customer.sales.order(created_at: :desc).limit(10)
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to customers_path, notice: "Cliente creado correctamente."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to customers_path, notice: "Cliente actualizado correctamente."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @customer.destroy
+      redirect_to customers_path, notice: "Cliente eliminado correctamente."
+    else
+      redirect_to customers_path, alert: @customer.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:name, :phone, :debt_limit)
   end
 end
